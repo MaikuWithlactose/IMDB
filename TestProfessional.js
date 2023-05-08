b355813ffaff7160a1e407b8d6ce4e55c004f650
MaikaNunez <Maikanunezolmos@gmail.com>
2023-05-08 20:10:32 +0200

    Correctivo del gitignore

--- TestProfessional.js
+++
@@ -1,6 +0,0 @@
-const { Professional } = require("./Professional.ts");  
-
-const actor = new Professional("Curro Romero", 47, 75, 1.83, false, "Español", 1, "Modelo, Cantante y Actor");
-
-// Llamando al método para mostrar las propiedades
-actor.MostrarProfessional();
